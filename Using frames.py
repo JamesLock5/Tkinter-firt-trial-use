fd27ebb7ceb2b90a9990ef06d929c68425f37165
JamesLock5 <jameslock5@icloud.com>
2026-01-14 10:16:17 +0000

    First use

---
+++ Using frames.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Using Frames')
+root.geometry('500x500')
+
+root.rowconfigure(0, weight = 1)
+root.rowconfigure(1, weight = 5)
+
+
+root.columnconfigure(0, weight = 1)
+root.columnconfigure(1, weight = 1)
+
+
+label1 = tk.Label(root, text= 'LABEL', relief = 'solid')
+label1.grid(row = 0, column = 0, columnspan = 3, sticky = 'ewsn', padx = 5, pady = 5)
+
+
+left_frame = tk.Frame(root, relief = 'solid', borderwidth = 5)
+left_frame.grid(row = 1, column = 0, sticky = 'nsew',  padx = 5, pady = 5)
+left_frame.rowconfigure(0, weight = 1)
+left_frame.grid_propagate(False)
+
+for i in range(3):
+    left_frame.rowconfiure
+right_frame = tk.Frame(root, relief = 'solid', borderwidth = 5)
+right_frame.grid(row = 1, column = 1, sticky = 'nsew', padx = 5, pady = 5)
+right_frame.rowconfigure(0, weight = 1)
+right_frame.grid_propagate(False)
+
+entry1 = tk.Entry(left_frame)
+entry1.grid(row = 0, column = 0)
+
+entry2 = tk.Entry(left_frame)
+entry2.grid(row = 1, column = 0)
+
+entry3 = tk.Entry(left_frame)
+entry3.grid(row = 2, column = 0)
+
+entry4 = tk.Entry(right_frame)
+entry4.grid(row = 0, column = 0)
+
+entry5 = tk.Entry(right_frame)
+entry5.grid(row = 1, column = 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
